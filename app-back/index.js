e207cdd9801595949d16a92ab33bfc094d987966
Redeltaz <lucascampistron95@gmail.com>
2023-07-10 15:12:55 +0200

    feat(app-back): init backend for main app

---
+++ app-back/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => {
+    res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
