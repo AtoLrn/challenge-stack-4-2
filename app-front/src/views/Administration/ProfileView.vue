53a3ffad77a5b2a1ed24938f00ad62c51198745a
Izïa <izia.crinier@gmail.com>
2023-07-25 09:16:50 +0200

    feat(app-front): update integration dashboard profile

--- app-front/src/views/Administration/ProfileView.vue
+++ app-front/src/views/Administration/ProfileView.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="right-side" class="flex flex-row content-se align-ctr">
-    <div class="flex flex-col align-ctr">
-      <img src="@/assets/img/profile.png" alt="profile picture" style="width: 300px;">
+    <div class="h-100 flex flex-col align-ctr content-se">
+      <div>
+        <img src="@/assets/img/profile.png" alt="profile picture" style="width: 300px;" />
+      </div>
+      <div id="kbis-container" class="card">
+        <button type="button" class="w-100 btn btn-md">Voir mon fichier Kbis</button>
+      </div>
     </div>
     <div class="flex flex-col">
       <div class="card">
         <form>
+          <div>
+            <p class="title c-indigo">
+              Informations personnelles
+            </p>
+          </div>
           <div class="flex flex-row content-sb">
             <div class="flex flex-col">
               <label for="lastname" class="caption c-purple">Nom</label>
@@ -16,26 +26,35 @@
               <input type="text" id="firstname" value="John" required>
             </div>
           </div>
-          <div class="flex flex-col">
+          <div class="flex flex-col bdr-btm">
             <label for="email" class="caption c-purple">Email</label>
             <input type="email" id="email" value="johndoe@gmail.com" required>
           </div>
-          <div class="flex flex-col">
-            <label for="password" class="caption c-purple">Password</label>
-            <input type="password" id="password" value="vzjzoijvozijeoizjioz" required>
+          <div>
+            <p class="title c-indigo">
+              Entreprise & Site
+            </p>
           </div>
           <div class="flex flex-col">
             <label for="company" class="caption c-purple">Nom de société</label>
             <input type="text" id="company" value="QuackMart" required>
           </div>
-          <div class="flex flex-col">
+          <div class="flex flex-col bdr-btm">
             <label for="url" class="caption c-purple">Url du site à analyser</label>
             <input type="text" id="url" value="https://quackmart.com" required>
           </div>
+          <div>
+            <p class="title c-indigo">
+              Modifier mon mot de passe
+            </p>
+          </div>
           <div class="flex flex-col">
-            <label for="kbis" class="caption c-purple">Kbis</label>
-            <input type="file" id="kbis" required style="display: none">
-            <button id="kbis-btn" class="btn btn-md">Choisir un fichier</button>
+            <label for="password" class="caption c-purple">Mot de passe actuel</label>
+            <input type="password" id="password" value="vzjzoijvozijeoizjioz" required>
+          </div>
+          <div class="flex flex-col">
+            <label for="new-password" class="caption c-purple">Nouveau mot de passe</label>
+            <input type="password" id="new-password" value="vzjzoijvozijeoizjioz" required>
           </div>
           <div id="card-bottom">
             <button type="submit" class="btn btn-md">Modifier mes informations</button>
@@ -64,10 +83,19 @@
   width: 40%;
 }
 
+#kbis-container {
+  text-align: center;
+  padding: 1.5rem;
+}
+
 .card {
   width: 80%;
   margin-left: auto;
   margin-right: auto;
+
+  .title {
+    font-size: 18px;
+  }
 
   form {
     padding: 1rem;
@@ -96,6 +124,10 @@
 
     }
 
+  }
+
+  .bdr-btm {
+    padding-bottom: 1rem;
   }
 }
 
