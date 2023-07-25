6d42c55fee7a3b953832fdd349d1900c5250921c
Izïa <izia.crinier@gmail.com>
2023-07-25 17:25:23 +0200

    feat(front-app): update request table

--- app-front/src/views/Administration/RequestsView.vue
+++ app-front/src/views/Administration/RequestsView.vue
@@ -1,10 +1,34 @@
+<template>
+  <div id="admin-panel" class="card">
+    <table class="table">
+      <thead>
+        <tr>
+          <th class="title">Nom</th>
+          <th class="title">Prénom</th>
+          <th class="title">Email</th>
+          <th class="title">Société</th>
+          <th class="title">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+          <td>johndoe@gmail.com</td>
+          <td>QuackMart</td>
+          <td>
+            <button class="btn btn-sm" style="margin-right: 0.25rem">Accepter &nbsp;<i class="fa-solid fa-circle-check"></i></button>
+            <button class="btn btn-sm">Refuser &nbsp;<i class="fa-solid fa-circle-xmark"></i></button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
 <script setup>
 
 </script>
-
-<template>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque inventore obcaecati officia sunt voluptatibus. Aperiam aspernatur dicta, distinctio eos pariatur repellat similique? Deserunt expedita, molestiae non porro repellat sit veniam.
-</template>
 
 <style scoped>
 
--- app-front/src/views/Administration/UsersView.vue
+++ app-front/src/views/Administration/UsersView.vue
@@ -2,25 +2,42 @@
   <div id="admin-panel" class="card">
     <table class="table">
       <thead>
-      <tr>
-        <th class="title">Nom</th>
-        <th class="title">Prénom</th>
-        <th class="title">Email</th>
-        <th class="title">Société</th>
-        <th class="title">Actions</th>
-      </tr>
+        <tr>
+          <th class="title">Nom</th>
+          <th class="title">Prénom</th>
+          <th class="title">Email</th>
+          <th class="title">Société</th>
+          <th class="title">Statut</th>
+          <th class="title">Actions</th>
+        </tr>
       </thead>
       <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>johndoe@gmail.com</td>
-        <td>QuackMart</td>
-        <td>
-          <button class="btn btn-sm" style="margin-right: 0.25rem">Modifier &nbsp;<i class="fa-solid fa-pen"></i></button>
-          <button class="btn btn-sm">Supprimer &nbsp;<i class="fa-solid fa-trash"></i></button>
-        </td>
-      </tr>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+          <td>johndoe@gmail.com</td>
+          <td>QuackMart</td>
+          <td>
+            <p class="tag tag-verified">Vérifié</p>
+          </td>
+          <td>
+            <button class="btn btn-sm" style="margin-right: 0.25rem">Modifier &nbsp;<i class="fa-solid fa-pen"></i></button>
+            <button class="btn btn-sm">Supprimer &nbsp;<i class="fa-solid fa-trash"></i></button>
+          </td>
+        </tr>
+        <tr>
+          <td>John</td>
+          <td>Doe</td>
+          <td>johndoe@gmail.com</td>
+          <td>QuackMart</td>
+          <td>
+            <p class="tag tag-pending">En attente</p>
+          </td>
+          <td>
+            <button class="btn btn-sm" style="margin-right: 0.25rem">Modifier &nbsp;<i class="fa-solid fa-pen"></i></button>
+            <button class="btn btn-sm">Supprimer &nbsp;<i class="fa-solid fa-trash"></i></button>
+          </td>
+        </tr>
       </tbody>
     </table>
   </div>
@@ -31,6 +48,22 @@
 </script>
 
 <style scoped lang="scss">
+  .tag {
+    padding: 4px 8px;
+    border-radius: 30px;
+    text-align: center;
+    font-weight: bold;
+    font-size: 12px;
 
+    &-verified {
+      background-color: var(--light-green);
+      color: var(--green);
+    }
 
+    &-pending {
+      background-color: var(--light-red);
+      color: var(--red);
+    }
+
+  }
 </style>
