2a747fec1ef9d8c96e2715bf4b7ca77f21c5ef13
Izïa <izia.crinier@gmail.com>
2023-07-26 19:50:16 +0200

    fix(app-client): fix integration tag modal

--- app-front/src/views/Administration/TagsView.vue
+++ app-front/src/views/Administration/TagsView.vue
@@ -8,31 +8,28 @@
       </div>
       <div class="w-50 text-r">
         <div v-if="activeTab === 'tags'">
-        <ModalAlert >
-          <template #activator="{ openModal }">
-            <button @click="openModal" 
-                    class="btn btn-purple btn-md">Ajouter un tag &nbsp;<i class="fa-solid fa-tag"></i>
-            </button>          
-          </template>
-          <template #actions="{ closeModal }">
-            <button title="close" @click="closeModal" class="btn btn-md">Fermer</button>
-          </template>
-          <template v-slot:title class="title">Ajouter un tag</template>
-          <template v-slot:default>
-            <form @submit.prevent="handleSubmit" class="flex flex-col align-ctr">
-              <div class="flex flex-col align-ctr">
-                <label for="description" class="caption c-purple">Description</label>
-                <input type="text" id="description" v-model.trim="descriptionTag" required>
-              </div>
-              <div id="card-bottom" class="flex flex-row align-ctr content-sb">
-                <div>
-                  <button type="submit" class="btn btn-md">Ajouter</button>
-                </div>
-              </div>
-            </form>
-          </template>
-        </ModalAlert>
-      </div>
+          <ModalAlert modalSize="w-25">
+            <template #activator="{ openModal }">
+              <button @click="openModal"
+                      class="btn btn-purple btn-md">Ajouter un tag &nbsp;<i class="fa-solid fa-tag"></i>
+              </button>
+            </template>
+            <template #actions="{ closeModal }">
+              <button title="close" @click="closeModal" class="btn btn-md">Fermer</button>
+            </template>
+            <template v-slot:title class="title">Ajouter un tag</template>
+            <template v-slot:default>
+              <form @submit.prevent="handleSubmit" id="form-tag" class="flex flex-row align-ctr content-sb">
+                    <input id="description"
+                           type="text"
+                           v-model.trim="descriptionTag"
+                           placeholder="Description"
+                           required/>
+                    <button type="submit" class="btn btn-sm btn-purple">Ajouter</button>
+              </form>
+            </template>
+          </ModalAlert>
+        </div>
         <button v-else
                 class="btn btn-purple btn-md">Ajouter un tunnel &nbsp;<i class="fa-solid fa-arrows-turn-right"></i>
         </button>
@@ -55,15 +52,13 @@
           <SankeyView :data="data" />
         </div>
 
-        <div  v-else>
-          <p>No data to display</p>
+        <div v-else>
+          <p>Aucune donnée à afficher</p>
         </div>
 
 
       </div>
     </div>
-
-
 
   </div>
 </template>
@@ -125,5 +120,18 @@ const handleSubmit = async () => {
   margin-top: 20px;
 }
 
+
+#form-tag {
+
+  input {
+    width: 70%;
+  }
+
+  button {
+    width: 25%;
+    padding: 8px;
+  }
+
+}
 </style>
 
